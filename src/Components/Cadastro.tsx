9260f61743727254ac3d985b75f2fbdbe4dbb850
MateusSartorio <mateussartorio39@gmail.com>
2022-10-04 11:18:06 -0300

    message

--- src/Components/Cadastro.tsx
+++ src/Components/Cadastro.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/sharp-solid-svg-icons";
+import { faXmark, faArrowRight } from "@fortawesome/sharp-solid-svg-icons";
 
 import { BotaoGenerico } from './BotaoGenerico';
 import { Logo } from './Logo'
@@ -17,19 +17,19 @@ export const Cadastro = (props: propsType) => {
 
             <div id="cadastro-info">
                 <Logo />
-                <h3>Cadastre-se no Urubu Investimentos</h3>
+                <h3>Cadastre-se no Urubu Investimentos!</h3>
                 
                 <form action="">
                     <input type="text" name="email" placeholder="Endereço de Email"/>
                     <input type="text" name="senha" placeholder="Senha"/>
                     <input type="number" name="idade" placeholder="Idade"/>
 
-                    <p>Ao se inscrever, você concorda com os <a href=".logo">Termos de serviço</a>.</p>
+                    <p id="termos-condicoes">Ao se inscrever, você concorda com os <a href=".logo">Termos de serviço</a>.</p>
 
-                    <BotaoGenerico texto="Começe já" href="#" fundo={true}/>
+                    <BotaoGenerico texto="Começe já! &rarr;" href="#" fundo={true}/>
                 </form>
 
-                <p>Já é um investidor? <a href=".logo">Entre</a>.</p>
+                <p id="ja-investidor">Já é um investidor? <a href=".logo">Entre</a>.</p>
             </div>
         <FontAwesomeIcon icon={faXmark} id="cadastro-x"/>
         </div>
