f03d8beb77a4412337a51efab000864fe17ffb68
MateusSartorio <mateussartorio39@gmail.com>
2022-11-16 16:40:45 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,12 @@ import { Inicial } from './Paginas/Inicial';
 
 import './App.css';
 
-const Home = () => {
-    return(<h1>Cool bro</h1>);        
-}
-
-const Another = () => {
-    return (<h1>Even cooler bro</h1>);        
-}
-
 export const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<Inicial />}/>
-                <Route path="blogs" element={<Another />} />
+                <Route path="blogs" element={<h1>Blogs</h1>} />
             </Routes>
         </BrowserRouter>
     );
--- src/Paginas/Inicial/index.tsx
+++ src/Paginas/Inicial/index.tsx
@@ -12,61 +12,47 @@ import "./index.css";
 
 export const Inicial = () => {
 
-  const [ showCadastro, setCadastro ] = useState(false);
-  const [ showEntrar, setEntrar ] = useState(false);
+	const [ showCadastro, setCadastro ] = useState(false);
+	const [ showEntrar, setEntrar ] = useState(false);
 
 
-  const fechaCadastro = () => {
-    setCadastro(false);
-  }
+	const fechaCadastro = () => {
+		setCadastro(false);
+	}
 
-  const abreCadastro = () => {
-    setCadastro(true);
-  }
+	const abreCadastro = () => {
+		setCadastro(true);
+	}
 
-  const fechaEntrar = () => {
-    setEntrar(false);
-  }
+	const fechaEntrar = () => {
+		setEntrar(false);
+	}
 
-  const abreEntrar = () => {
-    setEntrar(true);
-  }
+	const abreEntrar = () => {
+		setEntrar(true);
+	}
 
-  return (
-    <div className="App">
-      <Header
-        className='Urubu-logo'
-        abreCadastro={abreCadastro}
-        fechaCadastro={fechaCadastro}
-        abreEntrar={abreEntrar}
-        fechaEntrar={fechaEntrar}
-      />
-      <main>
-        <Sidebar />
-        <div id="main-content">
-          <MainCard titulo="Pacote de Boas Vindas" descricao='Ganhe coisas legais'/>
+	return (
+		<div className="App">
+			<Header
+				className='Urubu-logo'
+				abreCadastro={abreCadastro}
+				fechaCadastro={fechaCadastro}
+				abreEntrar={abreEntrar}
+				fechaEntrar={fechaEntrar}
+			/>
 
-          <Searchbar />
-        
-        {/*  <div id='small-cards-section'>
-            {smallcards.map((card, index) => {
-              const { titulo, descricao, imagem } = card;
-              return <SmallCard
-                        key={index}
-                        titulo={titulo}
-                        descricao={descricao}
-                        imagem={imagem}
-                      />
-            })}
-          </div>
-        */}
-        </div>
-      </main>
-      <Footer />
+            <Sidebar />
+            
+			<main>
+				<div id="main-content">
+					<MainCard titulo="Pacote de Boas Vindas" descricao='Ganhe coisas legais'/>		
+				</div>
+			</main>
 
-      { showEntrar && <Entrar fechaEntrar={fechaEntrar}/>}
-      { showCadastro && <Cadastro fechaCadastro={fechaCadastro}/> }
+			{ showEntrar && <Entrar fechaEntrar={fechaEntrar}/>}
+			{ showCadastro && <Cadastro fechaCadastro={fechaCadastro}/> }
 
-    </div>
-  );
+		</div>
+	);
 }
