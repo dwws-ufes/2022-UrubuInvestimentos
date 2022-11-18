80ff98e53d39b7f513f3492fdb18788758c7cd84
MateusSartorio <mateussartorio39@gmail.com>
2022-11-18 13:11:08 -0300

    mudancas na sidebar

--- src/Componentes/Sidebar/index.tsx
+++ src/Componentes/Sidebar/index.tsx
@@ -19,10 +19,36 @@ export const Sidebar = (props: propsType) => {
                 rel="noreferrer"
             >
                 <p>Parceiro Oficial</p>
-                <img src="/images/flamengo-logo-2020.svg" alt="Logo do Flamengo" />
+                <img src="/imagens/flamengo-logo-2020.svg" alt="Logo do Flamengo" />
             </a>
             
             <GanheDinheiro />
+
+            <BotaoGenerico
+                texto="Invista agora!"
+                href="#"
+                fundo={true}
+                callback={() => null}
+            />
+
+            <BotaoGenerico
+                texto="Jogos Passados"
+                href="#"
+                fundo={true}
+                callback={() => null}
+            />
+
+            <hr className="barra-horizontal"/>
+
+            <div className="links">
+                <ul>
+                    <li>INDIQUE UM</li>
+                    <li>REGRAS DE INVESTIMENTO</li>
+                    <li>SUPORTE AO VIVO</li>
+                    <li>PERGUNTAS FREQUENTES</li>
+
+                </ul>
+            </div>
         </div>
     );
 }
