0008ca6646e80dd4a649310ee88a39122c69011b
MateusSartorio <mateussartorio39@gmail.com>
2022-11-22 19:49:59 -0300

    mudancas feitas no perfil

--- src/Componentes/CardPerfil/index.tsx
+++ src/Componentes/CardPerfil/index.tsx
@@ -1,15 +1,57 @@
 import React from "react";
 
+import { FaRegUser, FaMoneyCheckAlt } from "react-icons/fa";
+import { GiReceiveMoney } from "react-icons/gi";
+import { BsCreditCard } from "react-icons/bs";
+import { IoCloseSharp } from "react-icons/io5";
+
+import { BotaoGenerico } from "../BotaoGenerico";
+
 import "./index.css";
 
 interface propsType {
-
+    nome: string;
+    saldo: string;
+    investimentos: number | string;
+    lucrou: string;
+    cartoes: string[];
 }
 
 export const CardPerfil = (props: propsType) => {
+    const { nome, saldo, investimentos, lucrou, cartoes } = props;
+
     return(
         <div className="card-perfil">
-            
+            <div className="parte-superior-perfil">
+                <FaRegUser className="logo-perfil"/>
+                <p>{nome}</p>
+                <p>${saldo}</p>
+            </div>
+            <div className="parte-meio-perfil">
+                <div>
+                   <FaMoneyCheckAlt className="icone-geral"/>
+                   <p>{investimentos} Investimentos</p>
+                </div>
+                <div>
+                    <GiReceiveMoney className="icone-geral"/>
+                    <p>Lucrou ${lucrou}</p>
+                </div>
+            </div>
+            <div className="parte-inferior-perfil">
+                <BotaoGenerico
+                    texto="Adicionar Cartão"
+                />
+
+                <div className="cartoes">
+                    {cartoes.map(cartao =>
+                        <div className="cartao-individual">
+                            <BsCreditCard className="icone-cartao-geral"/>
+                            <p>{cartao}</p>
+                            <IoCloseSharp className="icone-cartao-geral"/>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
--- src/Paginas/Perfil/index.tsx
+++ src/Paginas/Perfil/index.tsx
@@ -44,7 +44,13 @@ export const Perfil = () => {
             <main>
                 { sidebar && <Sidebar /> }
                 <section className="conteudo-principal">
-                    <CardPerfil /> 
+                    <CardPerfil
+                        nome="David Messias"
+                        saldo="10,00"
+                        investimentos={10}
+                        lucrou="-100,00"
+                        cartoes={["Cartão 1", "Cartão 2"]}
+                    /> 
                 </section>
             </main> 
             
