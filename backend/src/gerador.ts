265305e7a49ffabdb1b838f4fa3235edd6c0085c
YuriAikau <yuris5667@gmail.com>
2023-01-25 22:55:47 -0300

    Feito o gerador de números, pode mudar o intervalo de tempo mudando a variavel minutes

--- backend/src/gerador.ts
+++ backend/src/gerador.ts
@@ -1,25 +1,24 @@
-import express from "express";
+import axios from 'axios';
 
-const routes = require("./routes");
-var request = require('request');
+const generator = axios.create({
+    baseURL: "http://localhost:2222",
+});
 
-const generator = express();
+const minutes = 30;
 
-generator.use(express.json());
-generator.use(routes);
+const zeroPad = (num: number, places: number) => String(num).padStart(places,'0');
 
-generator.listen(6666);
+async function randomNumbers(){
+    const data = { 
+        number1: zeroPad(Math.floor(Math.random() * 10000),4),
+        number2: zeroPad(Math.floor(Math.random() * 10000),4),
+        number3: zeroPad(Math.floor(Math.random() * 10000),4),
+        number4: zeroPad(Math.floor(Math.random() * 10000),4),
+        number5: null
+    }
 
-while(1){
-    function randomNumbers(){
-        const data = {
-            number1: Math.floor(Math.random() * 10000),
-            number2: Math.floor(Math.random() * 10000),
-            number3: Math.floor(Math.random() * 10000),
-            number4: Math.floor(Math.random() * 10000)
-        }
+    console.log(data);
+    await generator.post('/', data);
+}   
 
-        var clientServerOptions
-    }   
-}
-
+setInterval(randomNumbers, minutes*(60000));
