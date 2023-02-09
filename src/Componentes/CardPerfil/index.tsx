b5578a906198eff9d8335400c4fa7208443d511d
YuriAikau <yuri.sanchez@edu.ufes.br>
2023-02-09 00:10:36 -0300

    adicionar saldo funcionando

--- src/Componentes/CardPerfil/index.tsx
+++ src/Componentes/CardPerfil/index.tsx
@@ -5,6 +5,10 @@ import { IoCloseSharp } from "react-icons/io5";
 
 import styles from "./index.module.css";
 import Content from "../../Content/Components/CardPerfil.json"
+import { useState } from "react";
+import api from "../../services/api";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSaldo, setSaldo } from "../../store/userInfoSlice";
 
 interface propsType {
     nome: string;
@@ -13,7 +17,33 @@ interface propsType {
 }
 
 export const CardPerfil = (props: propsType) => {
+    const investmentOwner = localStorage.getItem("profileId");
+
     const { nome, saldo, investimentos } = props;
+
+    const [ valor, setValor ] = useState(0);
+
+    const dispatch = useDispatch();
+
+    async function handleAddValor(e:any){
+        e.preventDefault();
+        e.target.reset();
+
+        const diff = saldo + valor;
+
+        try{
+            await api.put('/perfil', { balance: diff }, {
+                headers: {
+                    Authorization: investmentOwner,
+                }
+            });
+            
+            dispatch(setSaldo(diff));
+        }catch(err){
+            alert(err); 
+        }
+        
+    }
 
     return(
         <div className={styles.card_perfil}>
@@ -28,19 +58,20 @@ export const CardPerfil = (props: propsType) => {
                    <p>{investimentos} { Content.Investment }</p>
                 </div>
             </div>
-            <form className={styles.parte_inferior_perfil}>
+            <form
+                className={styles.parte_inferior_perfil}
+                onSubmit={handleAddValor}
+            >
                 <input
                     placeholder="$0.00"
                     type="text"
                     inputMode="numeric"
+                    onChange={e => setValor(Number(e.target.value))}
                 />
                 
                 <button
                     className={styles.btn}
                     type="submit"
-                    onClick={e => {
-                        e.preventDefault();
-                    }}
                 >
                     { Content.Card }
                 </button>
